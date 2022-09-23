b1436e8f71f60e98badf85f8616f387e0da3453a
Mohammed badr-eddine El housni <mel-hous@e2r10p13.1337.ma>
2022-09-23 18:17:50 +0100

    3

--- lex_search.c
+++ lex_search.c
@@ -6,7 +6,7 @@
 /*   By: mel-hous <mel-hous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 13:27:05 by mel-hous          #+#    #+#             */
-/*   Updated: 2022/09/23 16:22:43 by mel-hous         ###   ########.fr       */
+/*   Updated: 2022/09/23 17:39:35 by mel-hous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,14 +32,26 @@ t_token	lex_search(t_lexer	lexer)
 		token = token_cmpr(&lexer, ")", RIGHT_P);
 }
 
-t_token	string_collect(t_lexer lexer)
+t_token	string_collect(char	*s)
 {
 	t_token	token;
 	int		mode;
 	int		i;
+	int		len;
 
 	mode = 0;
-	i = 0;
+	len = 0;
+	i = ft_strlen(META_C);
+	while (*s && (mode != 0 || (!ft_strncmp(*s, META_C, i))))
+	{
+		mode = change_mode(*s);
+		len++;
+		s++;
+	}
+	if (!len)
+		return (t_init(ERROR, 0, NULL));
+	if (mode != 0 && *s == '\0')
+		return (t_init(ERROR, 0, NULL));
 }
 
 t_token	t_init(t_token_type	type, int len, char *p)
@@ -60,4 +72,13 @@ t_token	token_cmpr(t_lexer	*lexer, const char	*symbole, t_token_type type)
 	if (ft_strncmp(lexer->str, symbole, i))
 		return (t_init(ERROR, 0, NULL));
 	return (t_init(type, i, lexer->str));
+}
+
+int	change_mode(char c)
+{
+	if (c == '\'')
+		return (1);
+	if (c == '\"')
+		return (2);
+	return (0);
 }
--- lexer.h
+++ lexer.h
@@ -6,12 +6,14 @@
 /*   By: mel-hous <mel-hous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 14:08:59 by mel-hous          #+#    #+#             */
-/*   Updated: 2022/09/23 15:54:03 by mel-hous         ###   ########.fr       */
+/*   Updated: 2022/09/23 17:32:38 by mel-hous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEXER_H
 # define LEXER_H
+
+# define META_C = " \t\n|&()<>"
 
 typedef struct token
 {
@@ -32,8 +34,13 @@ typedef enum token_type
 	STRING,
 	ERROR,
 }		t_token_type;
+typedef enum s_error
+{
+	SYNTAX_ERR,
+	ERR,
+}		t_error;
 
-typedef struct lexer
+typedef struct s_lexer
 {
 	char	*full_str;
 	char	*str;
@@ -41,10 +48,10 @@ typedef struct lexer
 	t_error	error;
 }		t_lexer;
 
-typedef enum s_error
-{
-	SYNTAX_ERR,
-	ERR,
-}		t_error;
+int		change_mode(char c);
+t_token	token_cmpr(t_lexer	*lexer, const char	*symbole, t_token_type type);
+t_token	t_init(t_token_type	type, int len, char *p);
+t_token	lex_search(t_lexer	lexer);
+t_lexer	lex_init(char *s);
 
 #endif
