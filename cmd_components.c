337b5e68a12c1b99ffab4a1ae49b7956dd780b0b
Mohammed badr-eddine El housni <mel-hous@e2r13p4.1337.ma>
2022-09-30 17:39:53 +0100

    30/9-05:39

--- cmd_components.c
+++ cmd_components.c
@@ -6,13 +6,27 @@
 /*   By: mel-hous <mel-hous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 12:40:53 by mel-hous          #+#    #+#             */
-/*   Updated: 2022/09/28 12:42:58 by mel-hous         ###   ########.fr       */
+/*   Updated: 2022/09/30 17:38:55 by mel-hous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
-t_list cmd_ccomponents(t_lexer *lexer)
+t_list **cmd_ccomponents(t_lexer *lexer)
 {
-    
+    t_list	**elem;
+	t_list	*new;
+	t_token	token;
+
+    token = get_token(*lexer);
+	*elem = ft_lstnew(ft_substr(lexer->str, 0, token.len));
+	token = get_next_token(lexer);
+	while(token.type == WORD || token.type == VAR ||
+		token.type == WLDC)
+	{
+		new = ft_lstnew(ft_substr(lexer->str, 0, token.len));
+		ft_lstadd_back(elem, new);
+		token = get_next_token(lexer);
+	}
+	return (elem);
 }
--- collect_cmd.c
+++ collect_cmd.c
@@ -6,7 +6,7 @@
 /*   By: mel-hous <mel-hous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 14:45:13 by mel-hous          #+#    #+#             */
-/*   Updated: 2022/09/29 17:32:24 by mel-hous         ###   ########.fr       */
+/*   Updated: 2022/09/30 17:39:19 by mel-hous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,11 @@
 
 t_parser_node   *collect_cmd(t_lexer *lexer)
 {
-    t_list	*elem;
+    t_list	**elem;
 	t_token	token;
 
-	token = get_token(lexer);	
+	token = get_token(*lexer);	
 	if (token.type == WORD)
-	{
-		elem = ft_lstnew(ft_substr(lexer->str, 0, token.len));
-		token = get_next_token(lexer);
-		while(token.type == WORD)
-		{
-			ft_lstadd_back(elem, ft_substr(lexer->str, 0, token.len));
-			token = get_next_token(lexer);
-		}
-	}
-	
+		elem = cmd_ccomponents(lexer);
+	return (elem);
 }
