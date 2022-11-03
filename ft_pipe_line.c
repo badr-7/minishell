384e536b8abc3154dc9d8e662d55cd8c81bf5f0e
Your Name <you@example.com>
2022-11-03 09:52:31 +0100

    pppp

--- ft_pipe_line.c
+++ ft_pipe_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_pipe_line.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mel-hous <mel-hous@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 14:30:20 by mel-hous          #+#    #+#             */
-/*   Updated: 2022/11/01 13:50:49 by mel-hous         ###   ########.fr       */
+/*   Updated: 2022/11/03 09:50:03 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,13 @@ t_parser_node   *ft_pipe_line(t_lexer   *lexer)
 	if (cmd && cmd == MISSMATCH)
 	{
     	pipe = check_pipe(lexer);
-		if (!pipe)
-			return(cmd);
-		pipe->left = cmd;
+		if (pipe == MISMATCH)
+			return (cmd);
+		if (pipe)
+			pipe->left = cmd;
+		else
+			node_tree_clear(&cmd);
 		return (pipe);
-    }
-	return(cmd);
+	}
+	return (cmd);
 }
